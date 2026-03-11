80111a86820e1ae827d6fd408be244f36e2b97c7
MugTree <waterhose@protonmail.com>
2026-03-11 01:29:23 +0000

    wip

--- data/migrations/20260310192554_init.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-DROP TABLE IF EXISTS buildings;
-CREATE TABLE buildings (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR (50) UNIQUE NOT NULL
-);
--- +goose StatementEnd
-
--- data/migrations/20260310235722_add_jobs.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-DROP TABLE IF EXISTS jobs;
-CREATE TABLE jobs (
-    id SERIAL PRIMARY KEY,
-    description VARCHAR (50) UNIQUE NOT NULL,
-    building_id integer NOT NULL
-)
--- +goose StatementEnd
